fc686198e7090fdf5a97a841d4dc1808b28fe1bd
Daniel Kemp <daniel.kemp@live.com.au>
2014-08-18 18:14:08 +1000

    Fixed date support

--- types.go
+++ types.go
@@ -6,9 +6,11 @@ package opal
 
 import (
 	"database/sql/driver"
+	"database/sql"
 	"errors"
 	"fmt"
 	"reflect"
+	"strconv"
 	"time"
 )
 
@@ -23,6 +25,10 @@ var (
 	lockOpal bool
 	opal OpalMagic = opalMagic
 )
+
+// Borrowed from sql library
+var errNilPtr = errors.New("destination pointer is nil") // embedded in descriptive error
+
 
 // Allows a user to set their own id
 // Can only be run once
@@ -210,6 +216,11 @@ func (o *Int64) A(p int64) {
 	o.Int64 = &p
 }
 
+// Convenience setting method
+func (o *Int64) Assign(p int64) {
+	o.A(p)
+}
+
 // Int64 implements the Scanner interface.
 func (o *Int64) Scan(pValue interface{}) error {
 	if pValue == nil {
@@ -237,10 +248,32 @@ func (o *Int64) Scan(pValue interface{}) error {
 
 // Int64 implements the driver Valuer interface.
 func (o Int64) Value() (driver.Value, error) {
+	return o.I(), nil
+}
+
+func (o Int64) I() interface{} {
 	if o.Int64 == nil {
-		return nil, nil
+    	return nil
 	}
-	return *o.Int64, nil
+	return *o.Int64
+}
+
+func (o Int64) Interface() interface{} {
+	return o.I()
+}
+
+// Returns the primitive value of Int64 if nil returns 0
+// Same as Primitive()
+func (o Int64) P() int64 {
+	if v := o.I(); v != nil {
+		return v.(int64)
+	}
+	return 0 // TODO maybe return max instead
+}
+
+// Returns the primitive value of Int64 if nil returns 0
+func (o Int64) Primitive() int64 {
+	return o.P()
 }
 
 // Returns the primitive type
@@ -418,14 +451,19 @@ func (o *Time) A(p time.Time) {
 
 // Time implements the sql.Scanner interface.
 func (o *Time) Scan(pValue interface{}) error {
-	if value, ok := pValue.(time.Time); ok {
-		o.Time = new(time.Time)
-		*o.Time = value
-		return nil
-	}
 	if pValue == nil {
 		return nil
 	}
+	o.Time = new(time.Time)
+	switch value := pValue.(type) {
+	case time.Time:
+		*o.Time = value
+		return nil
+	case *time.Time:
+		o.Time = value
+		return nil
+	}
+	// TODO better support for time as it can be string, bytes pointers...
 	panic("Opal.Time: invalid value to scan into Time")
 	return nil
 }
@@ -455,4 +493,201 @@ func (o Time) String() string {
 
 func ABool(p bool) *bool {
 	return &p
+}
+
+// convertAssign copies to dest the value in src, converting it if possible.
+// An error is returned if the copy would result in loss of information.
+// dest should be a pointer type.
+func convertAssign(dest, src interface{}) error {
+	// Common cases, without reflect.
+	switch s := src.(type) {
+	case string:
+		switch d := dest.(type) {
+		case *string:
+			if d == nil {
+				return errNilPtr
+			}
+			*d = s
+			return nil
+		case *[]byte:
+			if d == nil {
+				return errNilPtr
+			}
+			*d = []byte(s)
+			return nil
+		}
+	case []byte:
+		switch d := dest.(type) {
+		case *string:
+			if d == nil {
+				return errNilPtr
+			}
+			*d = string(s)
+			return nil
+		case *interface{}:
+			if d == nil {
+				return errNilPtr
+			}
+			*d = cloneBytes(s)
+			return nil
+		case *[]byte:
+			if d == nil {
+				return errNilPtr
+			}
+			*d = cloneBytes(s)
+			return nil
+		case *sql.RawBytes:
+			if d == nil {
+				return errNilPtr
+			}
+			*d = s
+			return nil
+		}
+	case nil:
+		switch d := dest.(type) {
+		case *interface{}:
+			if d == nil {
+				return errNilPtr
+			}
+			*d = nil
+			return nil
+		case *[]byte:
+			if d == nil {
+				return errNilPtr
+			}
+			*d = nil
+			return nil
+		case *sql.RawBytes:
+			if d == nil {
+				return errNilPtr
+			}
+			*d = nil
+			return nil
+		}
+	}
+
+	var sv reflect.Value
+
+	switch d := dest.(type) {
+	case *string:
+		sv = reflect.ValueOf(src)
+		switch sv.Kind() {
+		case reflect.Bool,
+		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64:
+			*d = fmt.Sprintf("%v", src)
+			return nil
+		}
+	case *[]byte:
+		sv = reflect.ValueOf(src)
+		switch sv.Kind() {
+		case reflect.Bool,
+		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64:
+			*d = []byte(fmt.Sprintf("%v", src))
+			return nil
+		}
+	case *sql.RawBytes:
+		sv = reflect.ValueOf(src)
+		switch sv.Kind() {
+		case reflect.Bool,
+		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64:
+			*d = sql.RawBytes(fmt.Sprintf("%v", src))
+			return nil
+		}
+	case *bool:
+		bv, err := driver.Bool.ConvertValue(src)
+		if err == nil {
+			*d = bv.(bool)
+		}
+		return err
+	case *interface{}:
+		*d = src
+		return nil
+	}
+
+	if scanner, ok := dest.(sql.Scanner); ok {
+		return scanner.Scan(src)
+	}
+
+	dpv := reflect.ValueOf(dest)
+	if dpv.Kind() != reflect.Ptr {
+		return errors.New("destination not a pointer")
+	}
+	if dpv.IsNil() {
+		return errNilPtr
+	}
+
+	if !sv.IsValid() {
+		sv = reflect.ValueOf(src)
+	}
+
+	dv := reflect.Indirect(dpv)
+	if dv.Kind() == sv.Kind() {
+		dv.Set(sv)
+		return nil
+	}
+
+	switch dv.Kind() {
+	case reflect.Ptr:
+		if src == nil {
+			dv.Set(reflect.Zero(dv.Type()))
+			return nil
+		} else {
+			dv.Set(reflect.New(dv.Type().Elem()))
+			return convertAssign(dv.Interface(), src)
+		}
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		s := asString(src)
+		i64, err := strconv.ParseInt(s, 10, dv.Type().Bits())
+		if err != nil {
+			return fmt.Errorf("converting string %q to a %s: %v", s, dv.Kind(), err)
+		}
+		dv.SetInt(i64)
+		return nil
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		s := asString(src)
+		u64, err := strconv.ParseUint(s, 10, dv.Type().Bits())
+		if err != nil {
+			return fmt.Errorf("converting string %q to a %s: %v", s, dv.Kind(), err)
+		}
+		dv.SetUint(u64)
+		return nil
+	case reflect.Float32, reflect.Float64:
+		s := asString(src)
+		f64, err := strconv.ParseFloat(s, dv.Type().Bits())
+		if err != nil {
+			return fmt.Errorf("converting string %q to a %s: %v", s, dv.Kind(), err)
+		}
+		dv.SetFloat(f64)
+		return nil
+	}
+
+	return fmt.Errorf("unsupported driver -> Scan pair: %T -> %T", src, dest)
+}
+
+
+func asString(src interface{}) string {
+	switch v := src.(type) {
+	case string:
+		return v
+	case []byte:
+		return string(v)
+	}
+	return fmt.Sprintf("%v", src)
+}
+
+
+func cloneBytes(b []byte) []byte {
+	if b == nil {
+		return nil
+	} else {
+		c := make([]byte, len(b))
+		copy(c, b)
+		return c
+	}
 }
